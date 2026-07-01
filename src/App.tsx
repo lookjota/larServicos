aaf5e5e3713e80c6282dd0866d4d6c1c3224dc9e
lookjota <lookjota@gmail.com>
2026-07-01 19:13:30 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
@@ -12,7 +8,6 @@ import WhatsAppButton from './components/WhatsAppButton'
 import ServicosPremium from './components/ServicosPremium'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="px-4 md:px-16 pt-20 lg:px-32 ">
