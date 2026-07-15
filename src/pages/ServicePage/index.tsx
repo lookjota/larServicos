c78d575f97ce0d1822e10d026e63ef437d59d74c
lookjota <lookjota@gmail.com>
2026-07-14 23:52:49 -0300

    feat(refactor): Remove ServiceSectionsBuilder e simplifica renderizacao de paginas

--- src/pages/ServicePage/index.tsx
+++ src/pages/ServicePage/index.tsx
@@ -2,19 +2,15 @@ import PageRenderer from "../../components/PageRenderer";
 
 import type { ServicePageProps } from "./type"
 
-import { ServiceSectionsBuilder } from "../../domain/builders/ServiceSectionsBuilder";
 
 export default function ServicePage({
   service,
 }: ServicePageProps) {
 
-
-  const sections =
-    ServiceSectionsBuilder.build(service);
   return (
 
     <PageRenderer
-    sections={sections}
+    sections={service.sections}
     />
 
   );
